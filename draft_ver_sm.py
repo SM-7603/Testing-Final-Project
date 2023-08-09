a026b366932e945b821789dec1a54c06474aaad9
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-08-08 21:50:00 -0400

    add metadata for website operation

--- draft_ver_sm.py
+++ draft_ver_sm.py
@@ -3,3 +3,12 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+
+"""
+Meta Data:
+    - website link: https://demo.guru99.com/V4/
+    - Manager Info:
+        - user_id: mngr520521
+        - password: ajAtYhE
+"""
+
